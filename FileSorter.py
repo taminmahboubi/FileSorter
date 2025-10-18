5d77365a40ec26457ca7665bbd166b76ce225c14
Tamin Mahboubi <taminmahboubi@DESKTOP-FMC8PN8.localdomain>
2025-10-18 11:23:34 +0100

    Implement file sorting via extension

--- FileSorter.py
+++ FileSorter.py
@@ -1,5 +1,5 @@
-# provides a list of operating system functions (scandir,makedirs, etc.)
-import os
+import os # provides a list of operating system functions (scandir,makedirs, etc.)
+import shutil # handles file system operations safely (.move(), .rmtree())
 
 # ANSI color codes
 GREEN = '\033[92m'
@@ -30,4 +30,22 @@ for entry in unique_extensions: # for each entry in unique_extensions
     os.makedirs(entry, exist_ok=True) # make a directory for each entry, exist_ok=True allows for it to continue (not throw error) if one that directory already exists!
     created_dirs.append(entry) # add it to the list of created_dirs
     print(f"{YELLOW}Directory{RESET}: {entry} → {GREEN}CREATED{RESET}") # a print message to notify the user the directory was created.
- 
+
+
+ # 3) loop through all the files to get each file into their corresponding folders
+for entry in os.scandir('.'): # loop through the current directory again..
+    if entry.is_file(): # if the current entry is a file..
+        _, extension = os.path.splitext(entry.name) # set the name of the extension to 'extension', ignoring the filename with '_,'
+        clean_extension = extension[1:] # bascially removing the '.' from the extension
+
+        if clean_extension: # only move files with extensions
+            target_path = os.path.join(clean_extension, entry.name) # created the target path e.g. 'pdf/document.pdf'
+            shutil.move(entry.path, target_path) # moves the file from its current location(entry.path) to the new location(target_path)
+            print(f"Successfully Moved: {entry.name} to {GREEN}{clean_extension}{RESET}/")
+
+
+
+
+
+
+
